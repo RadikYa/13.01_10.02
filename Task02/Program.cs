ec44e3b5a0685fa98f81841939350f05f84fed4b
Radik Yaushev <sokkomp@mail.ru>
2023-01-15 12:28:06 +0300

    Решение Task02

--- Task02/Program.cs
+++ Task02/Program.cs
@@ -7,3 +7,16 @@
 // a = -9 b = -3 -> max = -3
 
 
+Console.WriteLine("Введите первое число");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+if (number1 > number2)
+{
+    Console.WriteLine($"Цифра {number1} больше чем {number2}");
+}
+else
+{
+    Console.WriteLine($"Цифра {number2} больше чем {number1}");
+}

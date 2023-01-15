5eab4ea4797112eff87e50a53904759e8ebd74f2
Radik Yaushev <sokkomp@mail.ru>
2023-01-15 11:44:50 +0300

    Add Task 03

--- Task01/Program.cs
+++ Task01/Program.cs
@@ -1,8 +1,49 @@
-﻿Console.WriteLine("Введите целое число: ");
-int number1 = Convert.ToInt32(Console.ReadLine());
-int number2 = Convert.ToInt32(Console.ReadLine());
+﻿console.WriteLine("введите целое число:");
+int number1 = Convert.ToInt32(Console.ReadLine()); //25 
+int number2 = Convert.ToInt32(Console.ReadLine()); // 2
 int square = number2 * number2;
 if (number1 == square)
 {
-    Console.WriteLine("Да")
+    Console.WriteLine("да");
 }
+else
+{
+    Console.WriteLine("нет");
+}
+
+
+// int number = Convert.ToInt32(Console.ReadLine());
+
+// if (number == 1 )
+// {
+//     Console.WriteLine("Понедельник");
+// }
+// else if (number == 2)
+// {
+//     Console.WriteLine("Вторник");
+// }
+// else if (number == 3)
+// {
+//     Console.WriteLine("Среда");
+// }
+// else if (number == 4)
+// {
+//     Console.WriteLine("Четверг");
+// }
+// else if (number == 5)
+// {
+//     Console.WriteLine("Пятница");
+// }
+// else if (number == 6)
+// {
+//     Console.WriteLine("Суббота");
+// }
+// else if (number == 7)
+// {
+//     Console.WriteLine("воскресенье");
+// }
+// else 
+// {
+//     Console.WriteLine("ERRor");
+// }
+// ;

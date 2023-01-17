f22ddd8fca62703c82a50bd5ce45dcae8afa13ae
Radik Yaushev <sokkomp@mail.ru>
2023-01-17 13:07:15 +0300

    Add 16

--- Task16/Program.cs
+++ Task16/Program.cs
@@ -9,7 +9,7 @@
 
 bool Square(int number1, int number2)
 {
-    return ((number1 * number1 == number2) || (number2 * number2 == number1));
+    return number1 * number1 == number2 || number2 * number2 == number1;
 }
 
 Console.Write("Введите число 1: ");
@@ -20,5 +20,11 @@ int number2 = Convert.ToInt32(Console.ReadLine());
 
 bool result = Square(number1, number2);
 
-if (result == true) Console.WriteLine("да");
-else Console.WriteLine("нет");
+// if (result) Console.WriteLine("да");
+// else Console.WriteLine("нет");
+
+string res = result ? "да" : "нет";
+
+Console.WriteLine(res);
+
+// Console.WriteLine(result ? "да" : "нет");

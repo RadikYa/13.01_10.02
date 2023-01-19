1f69e52e859f21717531b0d2761d16f3047f638e
Radik Yaushev <sokkomp@mail.ru>
2023-01-19 11:51:40 +0300

    Result Task 10

--- Task10/Program.cs
+++ Task10/Program.cs
@@ -6,3 +6,15 @@
 // 456 -> 5
 // 782 -> 8
 // 918 -> 1
+
+int numberSecond(int number)
+{
+    return (number / 10) % 10;
+}
+
+Console.Write("Введите трёхзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int result = numberSecond(number);
+
+Console.Write($"Вторая цифра: {result}");

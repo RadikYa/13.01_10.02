21a82c6c3df9d7a0888d3006c8bd4b99cfb49e87
Radik Yaushev <sokkomp@mail.ru>
2023-01-21 18:48:44 +0300

    Ended Taskr 23

--- Task23/Program.cs
+++ Task23/Program.cs
@@ -6,3 +6,20 @@
 
 // 3 -> 1, 8, 27
 // 5 -> 1, 8, 27, 64, 125
+
+
+Console.WriteLine("Введите натуральное число:");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number < 1) Console.WriteLine("Некорректный ввод");
+else Table(number);
+
+
+void Table(int num)
+{
+int count = 1;
+while (count <= num)
+{
+Console.WriteLine($"{count,5} {count * count * count,5}");
+count++;
+}
+}

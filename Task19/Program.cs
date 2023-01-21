3725a89a7e0b64582d72fffecd66bffb7f48a1a3
Radik Yaushev <sokkomp@mail.ru>
2023-01-21 21:04:39 +0300

    2 варианта решения Task 19: с пятизначным числом и любым числом

--- Task19/Program.cs
+++ Task19/Program.cs
@@ -11,31 +11,60 @@
 // 23432 -> да
 
 
-bool palindrome(int number)
+// bool palindrome(int number)
+// {
+//     int numberFirst = number / 10000;
+//     int numberSecond = (number / 1000) % 10;
+//     int numberFourth = (number / 10) % 10;
+//     int numberFifth = number % 10;
+//     return numberFirst == numberFifth && numberSecond == numberFourth;
+// }
+
+// Console.Write("Введите пятизначное число: ");
+// int number = Convert.ToInt32(Console.ReadLine());
+
+
+// if (number > 99999)
+// {
+//     Console.WriteLine($"Число {number} больше пятизначного числа");
+// }
+// else if (number < 10000)
+// {
+//     Console.WriteLine($"Число {number} меньше пятизначного числа");
+// }
+// else
+// {
+//     bool result = palindrome(number);
+
+//     if (result) Console.WriteLine("да");
+//     else Console.WriteLine("нет");
+// }
+
+
+
+long palindrome(long number)
 {
-    int numberFirst = number / 10000;
-    int numberSecond = (number / 1000) % 10;
-    int numberFourth = (number / 10) % 10;
-    int numberFifth = number % 10;
-    return numberFirst == numberFifth && numberSecond == numberFourth;
+    long numberInverted = 0;
+    long numberNow = number;
+    while (numberNow > 0)
+    {
+        long invertedNow = numberNow % 10;
+        numberInverted = numberInverted * 10 + invertedNow;
+        numberNow = numberNow / 10;
+    }
+    return (numberInverted);
 }
 
-Console.Write("Введите пятизначное число: ");
-int number = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите любое число: ");
+long number = Convert.ToInt64(Console.ReadLine());
 
+long result = palindrome(number);
 
-if (number > 99999)
+if (result == number)
 {
-    Console.WriteLine($"Число {number} больше пятизначного числа");
-}
-else if (number < 10000)
-{
-    Console.WriteLine($"Число {number} меньше пятизначного числа");
+    Console.WriteLine("Да");
 }
 else
 {
-    bool result = palindrome(number);
-
-    if (result) Console.WriteLine("да");
-    else Console.WriteLine("нет");
+    Console.WriteLine("Нет");
 }

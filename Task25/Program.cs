fd445372451854fdd3543d6b1d75cec82e8a47da
Radik Yaushev <sokkomp@mail.ru>
2023-01-25 11:58:46 +0300

    Add Task 27

--- Task25/Program.cs
+++ Task25/Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 25: Напишите 
+// 1. цикл, который принимает на вход два числа (A и B) и 
+// 2/ возводит число A в натуральную степень B.
+
+// 3, 5 -> 243 (3⁵)
+
+// 2, 4 -> 16

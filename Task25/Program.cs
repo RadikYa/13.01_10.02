a8c4f2e7ac5dc4ccbfb6c318bbfb28674c6c8916
Radik Yaushev <sokkomp@mail.ru>
2023-01-26 11:39:07 +0300

    Task25 Degree - Возведение в степень

--- Task25/Program.cs
+++ Task25/Program.cs
@@ -5,3 +5,33 @@
 // 3, 5 -> 243 (3⁵)
 
 // 2, 4 -> 16
+
+int Degree(int number, int numberDegree)
+{
+    int degreeNow = number;
+    for (int i = 1; i < numberDegree; i++)
+    {
+        degreeNow = degreeNow * number;
+    }
+    return degreeNow;
+}
+
+
+Console.WriteLine("Введите целое положительное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("В какую степень нужно возвести число, которое вы ввели: ");
+int numberDegree = Convert.ToInt32(Console.ReadLine());
+
+
+int result = Degree(number, numberDegree);
+
+if (numberDegree < 1)
+{
+    Console.WriteLine($"Степень должна быть больше нуля");
+}
+else
+{
+    Console.WriteLine($"Результат: {result}");
+}
+

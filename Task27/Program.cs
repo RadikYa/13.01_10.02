cf65a3d43db2d51b2b9867a7f01026de69451cc1
Radik Yaushev <sokkomp@mail.ru>
2023-01-26 13:14:50 +0300

    Task27 Sum - Сумма цифр в числе

--- Task27/Program.cs
+++ Task27/Program.cs
@@ -7,3 +7,31 @@
 // 82 -> 10
 
 // 9012 -> 12
+
+
+int Sum(int number)
+{
+    if (number < 0)
+    {
+        number = number * (-1); 
+    }
+    int[] arr = new int [number];
+    int sum = 0;
+    int numCurrent = number;
+    for (int i = 1; i < arr.Length; i++)
+    {
+        int numSum = numCurrent % 10; 
+        sum = sum + numSum;
+        numCurrent = numCurrent / 10;
+    }
+    return sum;
+}
+
+
+Console.WriteLine("Введите целое число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int result = Sum(number);
+
+
+Console.WriteLine($"Результат: {result}");

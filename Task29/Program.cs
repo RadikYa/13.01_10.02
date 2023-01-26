38cb8d56fada93a32d9dc4c7c6435c3c829e16f2
Radik Yaushev <sokkomp@mail.ru>
2023-01-26 15:52:10 +0300

    Task29 PrintArray - вывод рандомного массива

--- Task29/Program.cs
+++ Task29/Program.cs
@@ -3,3 +3,34 @@
 // 2. выводит их на экран в таком формате:
 
 // 1, 2, 5, 7, 19, 6, 1, 33 -> [1, 2, 5, 7, 19, 6, 1, 33]
+
+
+int[] GenerateArray(int size, int min, int max)
+{
+    int[] arr = new int[size];
+    Random rnd = new Random();
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rnd.Next(min, max + 1);
+    }
+    return arr;
+}
+
+void PrintArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length - 1; i++)
+    {
+        Console.Write($"{arr[i]}, ");
+    }
+    Console.Write($"{arr[7]} -> ");
+
+    Console.Write("[");
+    for (int i = 0; i < arr.Length - 1; i++)
+    {
+        Console.Write($"{arr[i]}, ");
+    }
+    Console.Write($"{arr[7]}]");
+}
+
+int[] array = GenerateArray(8, 0, 99);
+PrintArray(array);

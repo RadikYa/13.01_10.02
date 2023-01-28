06e8ccb90b56a2a7a647592b7533625124e9fbe4
Radik Yaushev <sokkomp@mail.ru>
2023-01-28 21:43:45 +0300

    Task 35 - Кол-во элементов в массиве

--- Task29/Program.cs
+++ Task29/Program.cs
@@ -44,3 +44,21 @@ void PrintArray(int[] arr)
 
 int[] array = GenerateArray(8, 0, 99);
 PrintArray(array);
+
+
+// void PrintArray (int[] array) 
+// {
+//    // Console.Write("[");
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         if(i < array.Length - 1) Console.Write($"{array[i]}, ");
+//         else Console.Write($"{array[i]}");
+//     }
+//     //Console.WriteLine("]");
+// }
+
+// int[] arr = CreateArrayRndInt(12, -9, 9);
+// PrintArray(arr);
+// System.Console.Write(" -> [");
+// PrintArray(arr);
+// System.Console.WriteLine("]");

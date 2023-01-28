1023c10c8fa2e625ba66f9000b271e0294726269
Radik Yaushev <sokkomp@mail.ru>
2023-01-28 22:46:11 +0300

    Add Task 34

--- Task01/Program.cs
+++ Task01/Program.cs
@@ -1,4 +1,4 @@
-﻿console.WriteLine("введите целое число:");
+﻿Console.WriteLine("введите целое число:");
 int number1 = Convert.ToInt32(Console.ReadLine()); //25 
 int number2 = Convert.ToInt32(Console.ReadLine()); // 2
 int square = number2 * number2;
---
+++ Task33/Program.cs
@@ -0,0 +1,49 @@
+﻿// Задача 33: Задайте массив. Напишите программу, которая
+// определяет, присутствует ли заданное число в массиве.
+// 4; массив [6, 7, 19, 345, 3] -> нет
+// 3; массив [6, 7, 19, 345, 3] -> да
+
+int[] CreateArrayRndInt(int size, int min, int max)
+{
+    int[] array = new int[size];
+    Random rnd = new Random();
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = rnd.Next(min, max + 1);
+    }
+
+    return array;
+}
+
+bool NumberTrueFalse(int number, int[] array)
+{
+  bool isNumber = false;
+  for (int i = 0; i < array.Length; i++)
+  {
+    if(array[i] == number) isNumber = true;
+  }
+  return isNumber;
+}
+
+void PrintArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length - 1) Console.Write($"{array[i]}, ");
+        else Console.Write($"{array[i]}");
+    }
+    Console.Write("] -> ");
+}
+
+
+int number = 4;
+int[] arr = CreateArrayRndInt(5, 1, 1000);
+Console.Write($"{number}; массив ");
+PrintArray(arr);
+bool numberTrueFalse = NumberTrueFalse(4,arr);
+Console.WriteLine($"{numberTrueFalse}");
+
+
+

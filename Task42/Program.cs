5bed5271dfd6121118b1e17387646b677c73541b
Radik Yaushev <sokkomp@mail.ru>
2023-01-31 12:30:59 +0300

    Add Task 44

--- Task42/Program.cs
+++ Task42/Program.cs
@@ -24,3 +24,24 @@ int number1 = Convert.ToInt32(Console.ReadLine());
 string decToBin = DecToBin(number1);
 
 Console.WriteLine(decToBin);
+
+
+// int DecimalToBinary(int num)
+// {
+//     int res = 0;
+//     int count = 1;
+//     while (num > 0)
+//     {
+//         res += num % 2 * count;   // 45 / 2 = 1, 1 + 0*10 = 1, 1 + 100 = 101 , 
+//         num /= 2;                   // 45 / 2 = 22 , 22 / 2 = 11 , 11 / 2 = 5 ,
+//         count *= 10;              // 10, 100 ,  1000
+//     }
+//     return res;
+
+// }
+
+// Console.Write("Введите число в десятично системе исчисления:");
+// int number = Convert.ToInt32(Console.ReadLine());
+
+// int decimalToBinary = DecimalToBinary(number);
+// Console.WriteLine($"{number} -> {decimalToBinary}");

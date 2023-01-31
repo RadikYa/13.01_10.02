96342b48fc16aef76eed57cfc22990a6d1b82c16
Radik Yaushev <sokkomp@mail.ru>
2023-01-31 11:27:04 +0300

    Add Task42

---
+++ Task42/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 42: Напишите программу, которая будет преобразовывать
+// десятичное число в двоичное.
+// 45 -> 101101
+// 3 -> 11
+// 2 -> 10
+
+
+string DecToBin(int num)
+{
+    string result = string.Empty;
+    while (num > 0)
+    {        
+        result = num % 2 + result; // от порядка операции зависит результат
+        num = num /2;
+    }
+
+    return result;
+}
+
+
+Console.Write("Введите десятичное число: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+string decToBin = DecToBin(number1);
+
+Console.WriteLine(decToBin);
